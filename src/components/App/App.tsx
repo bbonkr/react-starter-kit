b9327c00a16142ff961a5d32ec8fd377defa76a4
Pon Cheol Ku (구본철) <dev@bbon.kr>
2021-06-24 18:52:26 +0900

    Update dependencies #3
    - update dependencies

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,6 +5,7 @@ import {
     Route,
     Redirect,
 } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import { AsyncComponent, LoadingComponent } from '../AsyncComponent';
 
 const Header = AsyncComponent(() => import('../Header'), {
@@ -27,32 +28,36 @@ const PageNotFound = AsyncComponent(() => import('../PageNotFound'), {
     fallback: <LoadingComponent />,
 });
 
+const helmetContext = {};
+
 export const App = () => {
     return (
-        <Router>
-            <Header />
-            <Switch>
-                <Route path="/" exact>
-                    <Home />
-                </Route>
-                <Route path="/about" exact>
-                    <About />
-                </Route>
-                <Route path="/404" exact>
-                    <PageNotFound />
-                </Route>
-                <Route
-                    path="*"
-                    render={(props) => (
-                        <Redirect
-                            to={{
-                                pathname: '/404',
-                                state: { from: props.location },
-                            }}
-                        />
-                    )}
-                ></Route>
-            </Switch>
-        </Router>
+        <HelmetProvider context={helmetContext}>
+            <Router>
+                <Header />
+                <Switch>
+                    <Route path="/" exact>
+                        <Home />
+                    </Route>
+                    <Route path="/about" exact>
+                        <About />
+                    </Route>
+                    <Route path="/404" exact>
+                        <PageNotFound />
+                    </Route>
+                    <Route
+                        path="*"
+                        render={(props) => (
+                            <Redirect
+                                to={{
+                                    pathname: '/404',
+                                    state: { from: props.location },
+                                }}
+                            />
+                        )}
+                    ></Route>
+                </Switch>
+            </Router>
+        </HelmetProvider>
     );
 };
