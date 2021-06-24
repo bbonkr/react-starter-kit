6e7b75ae2be78b16daeeb4fd8f8a3a6ac5fea713
Pon Cheol Ku (구본철) <dev@bbon.kr>
2021-06-24 18:29:23 +0900

    Update dependencies #3
    - webpack: clean output when production environment

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -46,5 +46,6 @@ module.exports = {
         filename: '[name].js',
         path: path.join(path.resolve(__dirname, '..'), 'out', 'dist'),
         publicPath: 'dist/',
+        clean: true,
     },
 };
